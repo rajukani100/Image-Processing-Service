f39100b39e7ab9699b911beae7b3d7e17eeb6381
Raj Ukani <rajukani100@gmail.com>
2024-10-03 16:24:35 +0530

    initial mongodb connect

--- database/database.go
+++ database/database.go
@@ -1,1 +1,26 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+
+func ConnectMongoDB() {
+	var err error
+	client, err = mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Printf("error : %s", err.Error())
+		return
+	}
+
+	if err = client.Ping(context.TODO(), nil); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB.")
+}
