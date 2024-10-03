51027b5ef5b816f6aaee647adfd6d0d41b74b15b
Raj Ukani <rajukani100@gmail.com>
2024-10-03 18:16:25 +0530

    changes to HashPassword

--- services/password.go
+++ services/password.go
@@ -2,7 +2,7 @@ package services
 
 import "golang.org/x/crypto/bcrypt"
 
-func HashPassword(password *string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(*password), 14)
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
 }
