bc9915940337a5868284ede0cf41b178853a6280
Raj Ukani <rajukani100@gmail.com>
2024-10-03 18:20:11 +0530

    added register routes

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"Image-Processing-Service/controllers"
+	"Image-Processing-Service/database"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	//database connection
+	database.ConnectMongoDB()
+
+	//routes management
+	routes := gin.Default()
+	routes.POST("/register", controllers.Register)
+
+	routes.Run(":80")
 }
