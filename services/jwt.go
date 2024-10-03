23f2a89e0daae68b81d0a663701985c114ef6b96
Raj Ukani <rajukani100@gmail.com>
2024-10-03 17:45:58 +0530

    bug fixes

--- services/jwt.go
+++ services/jwt.go
@@ -6,15 +6,18 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
-var secret_key = "IAMBEASTWOW"
+var secret_key = []byte("IAMBEASTWOW")
 
-func GenerateJwt(username string) (string, error) {
+func GenerateJwt(username *string) (string, error) {
 	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"username": username,
+		"username": *username,
 		"iat":      time.Now().Unix(),
 		"exp":      time.Now().Add(24 * time.Hour).Unix(),
 	})
 	tokenString, err := claims.SignedString(secret_key)
-	return tokenString, err
+	if err != nil {
+		return "", err // signing error
+	}
+	return tokenString, nil //success
 
 }
