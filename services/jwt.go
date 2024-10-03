5fb6ebe0202ba521724ce8521f1d39e60e863ee2
Raj Ukani <rajukani100@gmail.com>
2024-10-03 22:29:32 +0530

    added verifyToken to authorize user

--- services/jwt.go
+++ services/jwt.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -20,4 +21,17 @@ func GenerateJwt(username *string) (string, error) {
 	}
 	return tokenString, nil //success
 
+}
+
+func VerifyToken(tokenString string) error {
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		return secret_key, nil
+	})
+	if err != nil {
+		return err
+	}
+	if !token.Valid {
+		return errors.New("invalid token")
+	}
+	return nil
 }
