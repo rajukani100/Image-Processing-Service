2b46297b17343716c84d2edc762781e88b29f243
Raj Ukani <rajukani100@gmail.com>
2024-10-04 13:51:24 +0530

    implemented GetImageInfoByID method to retrieve image model

--- database/image.go
+++ database/image.go
@@ -3,10 +3,30 @@ package database
 import (
 	"Image-Processing-Service/models"
 	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func SaveImageInfo(image *models.Image) error {
 	collection := client.Database("ImageProcessingService").Collection("images")
 	_, err := collection.InsertOne(context.Background(), *image)
 	return err
+}
+
+func GetImageInfoByID(id string) (*models.Image, error) {
+	objectId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
+
+	collection := client.Database("ImageProcessingService").Collection("images")
+
+	var image models.Image
+	err = collection.FindOne(context.Background(), bson.M{"_id": objectId}).Decode(&image)
+	if err != nil {
+		return nil, err
+	}
+	return &image, nil
+
 }
