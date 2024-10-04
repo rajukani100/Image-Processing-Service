0e6e9c197d8cee716f29fd6084e39df463f80a5f
Raj Ukani <rajukani100@gmail.com>
2024-10-04 11:15:52 +0530

    added Authorized rotes and some additional setup

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"Image-Processing-Service/controllers"
 	"Image-Processing-Service/database"
+	"Image-Processing-Service/middleware"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,8 +14,21 @@ func main() {
 
 	//routes management
 	routes := gin.Default()
+	routes.MaxMultipartMemory = 8 << 20 // 8 MB
+	routes.RedirectTrailingSlash = false
+
+	// Public routes
 	routes.POST("/register", controllers.Register)
 	routes.POST("/login", controllers.Login)
+	routes.Static("/uploads", "./assets/uploads")
+	routes.Static("/edited", "./assets/edited")
+
+	//Authorized routes
+	authorized := routes.Group("/images")
+	authorized.Use(middleware.JwtAuth)
+	{
+		authorized.POST("", controllers.UploadImage) // /image routes
+	}
 
 	routes.Run(":80")
 }
